0cc5c54fe154d9c7f75b4e750339dc884369c247
Johan Dufau <johan.dufau@creatiwity.net>
2020-06-10 15:01:44 +0200

    [Axios] Fixes back URL

--- nuxt.config.js
+++ nuxt.config.js
@@ -64,7 +64,7 @@ export default {
    ** See https://axios.nuxtjs.org/options
    */
   axios: {
-    baseURL: process.env.BASE_URL
+    baseURL: process.env.BACK_URL
   },
   /*
    ** Build configuration
