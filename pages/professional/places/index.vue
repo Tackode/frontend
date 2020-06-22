fb330ac6782ac2a86e875408d10ee8a5d723f55d
Théo Leleu <theo_leleu@hotmail.fr>
2020-06-22 18:18:15 +0200

    [Place]Add delete and modify button

--- pages/professional/places/index.vue
+++ pages/professional/places/index.vue
@@ -28,17 +28,38 @@
           >Show QRCode</nuxt-link
         >
       </template>
+      <template v-slot:cell(modify)>
+        <b-button
+          v-b-modal.place-creation-modal
+          size="lg"
+          variant="primary"
+          class="mb-2"
+        >
+          <b-icon icon="pencil"></b-icon>
+        </b-button>
+      </template>
+
+      <template v-slot:cell(delete)>
+        <b-button
+          v-b-modal.place-delete-modal
+          size="lg"
+          variant="primary"
+          class="mb-2"
+        >
+          <b-icon icon="trash"></b-icon>
+        </b-button>
+      </template>
     </b-table>
     <p v-else>
       You don't have any places for now. Please, create a place to begin.
     </p>
 
     <b-modal id="place-creation-modal" title="Place Creation">
-      <b-form @submit="handlePlaceCreationSubmit">
+      <b-form @submit="handleplaceFormValuesSubmit">
         <b-form-group label="Name:" label-for="place-name">
           <b-form-input
             id="place-name"
-            v-model="placeCreation.name"
+            v-model="placeFormValues.name"
             required
             placeholder="Name"
           ></b-form-input>
@@ -47,7 +68,7 @@
         <b-form-group label="Description:" label-for="place-description">
           <b-form-input
             id="place-description"
-            v-model="placeCreation.description"
+            v-model="placeFormValues.description"
             required
             placeholder="Description"
           ></b-form-input>
@@ -59,15 +80,25 @@
         >
           <b-form-input
             id="place-average-duration"
-            v-model="placeCreation.averageDuration"
+            v-model="placeFormValues.averageDuration"
             required
             placeholder="Average Duration"
           ></b-form-input>
         </b-form-group>
-
         <b-button type="submit" variant="primary">Create</b-button>
       </b-form>
 
+      <template v-slot:modal-footer>
+        <span></span>
+      </template>
+    </b-modal>
+
+    <b-modal id="place-delete-modal" title="Delete Place">
+      <b-form @submit="handleplaceFormValuesDelete">
+        Do you really want to delete the place?
+        <b-button type="submit" variant="success">Yes</b-button>
+        <b-button type="cancel" variant="danger">No</b-button>
+      </b-form>
       <template v-slot:modal-footer>
         <span></span>
       </template>
@@ -81,7 +112,7 @@ import Component from 'vue-class-component'
 import { showError } from '../../../helpers/alerts'
 import { Place } from '../../../types/Place'
 
-interface PlaceCreation {
+interface PlaceFormValues {
   name: string
   description: string | null
   averageDuration: string
@@ -89,10 +120,10 @@ interface PlaceCreation {
 
 @Component
 export default class ProfessionalPlaces extends Vue {
-  fields = ['place_name', 'average_duration', 'action']
+  fields = ['place_name', 'average_duration', 'action', 'modify', 'delete']
   places: Place[] = []
-
-  placeCreation: PlaceCreation = {
+  placeFormMode: Boolean = false
+  placeFormValues: PlaceFormValues = {
     name: '',
     description: null,
     averageDuration: '',
@@ -119,16 +150,16 @@ export default class ProfessionalPlaces extends Vue {
     }
   }
 
-  async handlePlaceCreationSubmit(e: Event) {
+  async handleplaceFormValuesSubmit(e: Event) {
     e.preventDefault()
 
     try {
       await this.$axios.$post(
         '/place',
         {
-          name: this.placeCreation.name,
-          description: this.placeCreation.description,
-          averageDuration: parseInt(this.placeCreation.averageDuration),
+          name: this.placeFormValues.name,
+          description: this.placeFormValues.description,
+          averageDuration: parseInt(this.placeFormValues.averageDuration),
         },
         {
           auth: {
@@ -147,6 +178,62 @@ export default class ProfessionalPlaces extends Vue {
     }
 
     this.$bvModal.hide('place-creation-modal')
+
+    this.loadData()
+  }
+
+  async handleplaceFormValuesModify(e: Event) {
+    e.preventDefault()
+
+    try {
+      await this.$axios.$put(
+        '/place',
+        {
+          name: this.placeFormValues.name,
+          description: this.placeFormValues.description,
+          averageDuration: parseInt(this.placeFormValues.averageDuration),
+        },
+        {
+          auth: {
+            username: this.$store.getters['session/login'],
+            password: this.$store.getters['session/token'],
+          },
+        }
+      )
+    } catch (error) {
+      showError(
+        this.$bvToast,
+        'Connexion',
+        new Error('A network error has occurred. Please, try again.')
+      )
+      return
+    }
+
+    this.$bvModal.hide('place-creation-modal')
+
+    this.loadData()
+  }
+
+  async handleplaceFormValuesDelete(e: Event) {
+    e.preventDefault()
+
+    try {
+      await this.$axios.$delete('/place/' + this.placeFormValues.name, {
+        auth: {
+          username: this.$store.getters['session/login'],
+          password: this.$store.getters['session/token'],
+        },
+      })
+    } catch (error) {
+      showError(
+        this.$bvToast,
+        'Connexion',
+        new Error('A network error has occurred. Please, try again.')
+      )
+      return
+    }
+
+    this.$bvModal.hide('place-delete-modal')
 
     this.loadData()
   }
