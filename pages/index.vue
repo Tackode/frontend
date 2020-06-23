3f3efc1dc0cd244a0ba6986c6f93ec06005db621
Théo Leleu <theo_leleu@hotmail.fr>
2020-06-23 17:09:46 +0200

    Login Message in HomePage

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,23 @@
 <template>
   <div class="container">
     <div>
-      <h1>Welcome to Covid Journal!</h1>
       <br />
+      <br />
+      <h1>Welcome to Covid Journal!</h1>
+      Be aware of Covid in public and private places
+      <br />
+      <h2>Loggin In</h2>
+      <b-button to="/login" variant="primary">User log in</b-button>
 
+      <b-button to="/login-professional" variant="primary"
+        >Professional log in</b-button
+      >
+      <br />
+      <br />
+      <h2>Check in a place</h2>
+      Scan a QR Code
+      <br />
+      <br />
       <b-button to="/check-in" variant="primary">Do a Check-in</b-button>
     </div>
   </div>
