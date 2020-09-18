61f86ccd70fb7569a2b090064b995ea94b507635
Julien Blatecky <julien.blatecky@creatiwity.net>
2020-09-18 10:59:54 +0200

    Fix syntax

--- pages/index.vue
+++ pages/index.vue
@@ -158,7 +158,7 @@
     "step-3-title": "Étape 3",
     "step-3": "Vous êtes notifié par mail si vous avez croisé une personne infectée par le Covid-19 dans cet établissement",
     "cost-paragraph-1": "Le coronavirus est l'affaire de tous, ce qui signifie que chacun d'entre nous doit faire un effort pour que nous puissions sortir tous ensemble de cette pandémie mondiale.",
-    "cost-paragraph-2": "C'est pour cette raison que nous avons décidé pour le moment d'offrir gratuitement notre application à toutes les personnes qui souhaitent en profiter et de permettre ainsi à un maximum de personne de pouvoir sortir et profiter en toute sécurité !",
+    "cost-paragraph-2": "C'est pour cette raison que nous avons décidé pour le moment d'offrir gratuitement notre application à toutes les personnes qui souhaitent en profiter et de permettre ainsi à un maximum de personnes de pouvoir sortir et profiter en toute sécurité !",
 
     "register":"Enregistrez un lieu de passage pour être informé en cas de signalement d'une infection.",
     "introduction":"Gérez votre organisation et les lieux correspondants.",
