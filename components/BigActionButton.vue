af10ccc5c8458f3b18b80b57b6176bc3d2d6228c
Ludovic Douziech <ludovic.douziech@gmail.com>
2020-10-06 19:36:08 +0200

    [BigActionButton] Responsive: always display image

--- components/BigActionButton.vue
+++ components/BigActionButton.vue
@@ -1,22 +1,27 @@
 <template>
   <b-card class="hoverable-card">
-    <b-media class="align-items-center">
-      <template v-if="image" v-slot:aside class="d-none d-md-block">
-        <b-img
-          :src="require(`@/assets/images/${image}.png`)"
-          :srcset="`${require(`@/assets/images/${image}.png`)}    1x,
+    <ul class="no-dot">
+      <b-media tag="li" class="align-items-center">
+        <template v-if="image" v-slot:aside>
+          <b-img
+            :src="require(`@/assets/images/${image}.png`)"
+            :srcset="`${require(`@/assets/images/${image}.png`)}    1x,
                 ${require(`@/assets/images/${image}@2x.png`)} 2x`"
-          class="btn-icon d-none d-md-block"
-        />
-      </template>
-      <h5 class="mt-0">
-        <nuxt-link v-if="url" :to="url" class="stretched-link">{{
-          title
-        }}</nuxt-link>
-        <p v-else>{{ title }}</p>
-      </h5>
-      <p class="mb-0">{{ subtitle }}</p>
-    </b-media>
+            class="btn-icon"
+          />
+        </template>
+        <h5 class="mt-0">
+          <nuxt-link v-if="url" :to="url" class="stretched-link">{{
+            title
+          }}</nuxt-link>
+          <p v-else>{{ title }}</p>
+        </h5>
+        <p class="mb-0 subtitle">{{ subtitle }}</p>
+      </b-media>
+      <b-media tag="li" class="align-items-center subtitle-responsive">
+        <p class="mb-0">{{ subtitle }}</p>
+      </b-media>
+    </ul>
   </b-card>
 </template>
 
@@ -47,11 +52,35 @@ export default class BigActionButton extends BigButtonProps {}
   max-height: 80px;
 }
 
-.hoverable-card a {
-  color: rgb(33, 37, 41);
+.no-dot {
+  padding: 0;
+  list-style-type: none;
+}
 
-  &:hover {
-    text-decoration: none;
+.hoverable-card {
+  a {
+    color: rgb(33, 37, 41);
+
+    &:hover {
+      text-decoration: none;
+    }
+  }
+
+  @media (min-width: 576px) {
+    .subtitle-responsive {
+      display: none;
+    }
+  }
+
+  @media (max-width: 576px) {
+    .btn-icon {
+      max-width: 60px;
+      max-height: 60px;
+    }
+
+    .subtitle {
+      display: none;
+    }
   }
 }
 </style>
